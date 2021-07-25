48cc87ceb4eda0c90aebd5f465a04c64556b44dd
WDEXcourse <wdkawasaki.online@gmail.com>
2021-07-25 11:23:39 +0900

    ChooseColorAgainのプログラム

---
+++ Assets/SceneSwitchingAgain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchingAgain : MonoBehaviour
+{
+    public static string beforeSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickGetScene()
+    {
+        beforeSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    public void OnClickSwitchingScene()
+    {
+        SceneManager.LoadScene(beforeSceneName);
+    }
+}
